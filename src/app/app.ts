8212ef30e4a82efd4cb3bfcb0540d63a3880d3a9
saurabhkharche1234 <saurabhkharche1234@gmail.com>
2026-06-28 20:43:25 +0530

    Added yes no animation

--- src/app/app.ts
+++ src/app/app.ts
@@ -10,10 +10,15 @@ import { CommonModule } from '@angular/common';
 export class App {
   protected readonly title = signal('vat-purnima');
   showMessage = false;
+  messageStep = 0;
+  noButtonTop = 20;
+  noButtonLeft = 160;
   private audio?: HTMLAudioElement;
 
   surprise() {
     this.showMessage = true;
+    this.messageStep = 1;
+    this.resetNoButtonPosition();
 
     if (typeof Audio === 'undefined') {
       return;
@@ -31,7 +36,38 @@ export class App {
     }
   }
 
+  sendLove() {
+    this.messageStep = 2;
+    this.resetNoButtonPosition();
+  }
+
+  sayYes() {
+    this.messageStep = 3;
+    setTimeout(() => {
+      this.messageStep = 4;
+    }, 2000);
+  }
+
+  moveNoButton() {
+    if (this.messageStep !== 2) {
+      return;
+    }
+
+    this.noButtonTop = this.randomInt(0, 90);
+    this.noButtonLeft = this.randomInt(0, 220);
+  }
+
   hideMessage() {
     this.showMessage = false;
+    this.messageStep = 0;
+  }
+
+  private resetNoButtonPosition() {
+    this.noButtonTop = 20;
+    this.noButtonLeft = 160;
+  }
+
+  private randomInt(min: number, max: number) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 }
